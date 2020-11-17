2a1a1cb3592cb8f6b8f1bca10daf46629397af4b
wg12385 <will.gerrard@bristol.ac.uk>
2020-11-17 13:27:54 +0000

    scripts init

---
+++ make_conda_env.sh
@@ -0,0 +1,10 @@
+version=v1.0
+conda env remove -n imp_predictor_${version}
+conda create -n imp_predictor_${version} python=3 -y
+source activate imp_predictor_${version}
+conda install -n imp_predictor_${version} numpy scipy pandas -y
+conda install -n imp_predictor_${version} -c openbabel openbabel -y
+conda install -n imp_predictor_${version} -c rdkit rdkit -y
+conda install -n imp_predictor_${version} -c conda-forge tqdm -y
+conda install -n imp_predictor_${version} pytest
+pip install git+https://github.com/qmlcode/qml@develop --user -U
