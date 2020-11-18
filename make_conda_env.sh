3638ef1ac69d1dc54fa38266972f195ac151740e
wg12385 <will.gerrard@bristol.ac.uk>
2020-11-18 16:47:42 +0000

    v2.0 init

--- make_conda_env.sh
+++ make_conda_env.sh
@@ -1,10 +1,12 @@
-version=v1.0
+version=v2.0
 conda env remove -n imp_predictor_${version}
 conda create -n imp_predictor_${version} python=3 -y
 source activate imp_predictor_${version}
 conda install -n imp_predictor_${version} numpy scipy pandas -y
+conda install -n imp_predictor_${version} pytorch torchvision -y # CPU VERSION
 conda install -n imp_predictor_${version} -c openbabel openbabel -y
 conda install -n imp_predictor_${version} -c rdkit rdkit -y
-conda install -n imp_predictor_${version} -c conda-forge tqdm -y
-conda install -n imp_predictor_${version} pytest
-pip install git+https://github.com/qmlcode/qml@develop --user -U
+conda install -n imp_predictor_${version} -c conda-forge tqdm cytoolz -y
+conda install -n imp_predictor_${version} -c dglteam dgl -y # CPU VERSION
+conda install -n imp_predictor_${version} requests -y
+conda install -n imp_predictor_${version} pytest -y
