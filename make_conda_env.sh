b90fdf68504556174e9ace4ce02009e067fb6462
Max Williams <me@maxwillia.ms>
2020-12-07 18:31:51 +0100

    add install xorg-libxrender for openbabel to conda environment

--- make_conda_env.sh
+++ make_conda_env.sh
@@ -4,6 +4,7 @@ conda create -n imp_predictor_"${version}" python=3 -y
 source activate imp_predictor_"${version}"
 conda install -n imp_predictor_"${version}" numpy scipy pandas -y
 conda install -n imp_predictor_"${version}" -c openbabel openbabel -y
+conda install -c conda-forge xorg-libxrender -y
 conda install -n imp_predictor_"${version}" -c rdkit rdkit -y
 conda install -n imp_predictor_"${version}" -c conda-forge tqdm -y
 conda install -n imp_predictor_"${version}" pytest
