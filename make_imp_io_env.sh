cce068cf258c77df9b81aee3aa7a86a3b7baa0eb
Max Williams <me@maxwillia.ms>
2020-12-16 11:49:56 +0100

    Split make_conda_env.sh and remove v1.0 from environment names
    Bringing webstable branch (with some changes from v1.0-dev) into alignment with current tooling for gcloud submission.

---
+++ make_imp_io_env.sh
@@ -0,0 +1,10 @@
+set -e
+
+conda env remove -n imp_io
+conda create -n imp_io python=3 -y
+conda install -n imp_io numpy scipy pandas -y
+conda install -n imp_io -c openbabel openbabel -y
+conda install -n imp_io conda-forge xorg-libxrender -y
+conda install -n imp_io -c rdkit rdkit -y
+conda install -n imp_io -c conda-forge tqdm -y
+conda install -n imp_io pytest -y
--- predict.sh
+++ predict.sh
@@ -1,4 +1,4 @@
-source activate imp_io_v1.0
+source activate imp_io
 mkdir tmp
 echo Converting input molecules ...
 python setup_pred.py
@@ -9,7 +9,7 @@ then
     return ${exit_status} 2>/dev/null || exit "${exit_status}"
 fi
 conda deactivate
-source activate imp_core_v1.0
+source activate imp_core
 echo Making predictions ...
 python do_predict.py
 # Read exit status and attempt to exit if not 0
@@ -19,7 +19,7 @@ then
     return ${exit_status} 2>/dev/null || exit "${exit_status}"
 fi
 conda deactivate
-source activate imp_io_v1.0
+source activate imp_io
 echo Converting output molecules ...
 python output_pred.py
 # Read exit status and attempt to exit if not 0
